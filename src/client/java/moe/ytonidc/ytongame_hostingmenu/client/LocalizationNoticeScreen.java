dc749edb6345005b13b7f2b9b49cbd3d673ed63b
Kotori0629 <79943751@qq.com>
2026-03-01 20:13:05 +0800

    fix: 修复模糊遮罩覆盖文字的渲染问题

--- src/client/java/moe/ytonidc/ytongame_hostingmenu/client/LocalizationNoticeScreen.java
+++ src/client/java/moe/ytonidc/ytongame_hostingmenu/client/LocalizationNoticeScreen.java
@@ -97,7 +97,7 @@ public class LocalizationNoticeScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
 
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 15, 0xFFFFFF);
 
@@ -111,8 +111,6 @@ public class LocalizationNoticeScreen extends Screen {
             }
             textY += lineHeight;
         }
-
-        super.render(context, mouseX, mouseY, delta);
     }
 
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
