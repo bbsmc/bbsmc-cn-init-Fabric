5b82e563363c69eaabc2899936b2b8442db6b26e
Kotori0629 <79943751@qq.com>
2026-03-01 21:04:50 +0800

    fix: 修复 section sign 转义和缺失的常量声明

--- src/client/java/moe/ytonidc/ytongame_hostingmenu/client/LocalizationNoticeScreen.java
+++ src/client/java/moe/ytonidc/ytongame_hostingmenu/client/LocalizationNoticeScreen.java
@@ -14,18 +14,20 @@ import java.util.List;
 
 public class LocalizationNoticeScreen extends Screen {
 
-    private static final String TITLE_TEXT = "\§l\§eBBSMC汉化包使用须知";
+    private static final String TITLE_TEXT = "\u00a7l\u00a7eBBSMC汉化包使用须知";
     private static final String[] NOTICE_LINES = {
-        "\§f感谢您选择BBSMC汉化包，在正式使用BBSMC汉化包进行游戏前，我们希望您能阅读以下内容。",
+        "\u00a7f感谢您选择BBSMC汉化包，在正式使用BBSMC汉化包进行游戏前，我们希望您能阅读以下内容。",
         "",
-        "\§6\§l一、汉化内容",
-        "\§fBBSMC汉化包通过AI翻译+人工精校生成，汉化内容势必存在少量问题，如果您发现了任何汉化质量问题，您可以前往我们的网站：\§n\§bhttps://bbsmc.net\§r\§f，加入我们的玩家QQ群进行反馈，我们会及时处理您的反馈，并重新发布修改后的汉化包。",
-        "\§f若您当前游玩的整合包已经有完整的人工翻译，我们也相当欢迎您使用更精准更优质的人工翻译。",
+        "\u00a76\u00a7l一、汉化内容",
+        "\u00a7fBBSMC汉化包通过AI翻译+人工精校生成，汉化内容势必存在少量问题，如果您发现了任何汉化质量问题，您可以前往我们的网站：\u00a7n\u00a7bhttps://bbsmc.net\u00a7r\u00a7f，加入我们的玩家QQ群进行反馈，我们会及时处理您的反馈，并重新发布修改后的汉化包。",
+        "\u00a7f若您当前游玩的整合包已经有完整的人工翻译，我们也相当欢迎您使用更精准更优质的人工翻译。",
         "",
-        "\§6\§l二、广告内容",
-        "\§fBBSMC汉化包含有仅出现在服务器多人列表和创建世界导航标签页面的服务器广告，不会对游戏体验造成影响。我们需要一定的收入来支撑汉化服务器的运转。\§c\§l如果觉得广告影响游戏体验，请先点击\"拒绝并退出\"关闭游戏，然后手动删除mods文件夹内的YTGame-HostingMenu.jar文件，再重新启动游戏即可。",
+        "\u00a76\u00a7l二、广告内容",
+        "\u00a7fBBSMC汉化包含有仅出现在服务器多人列表和创建世界导航标签页面的服务器广告，不会对游戏体验造成影响。我们需要一定的收入来支撑汉化服务器的运转。\u00a7c\u00a7l如果觉得广告影响游戏体验，请先点击\"拒绝并退出\"关闭游戏，然后手动删除mods文件夹内的YTGame-HostingMenu.jar文件，再重新启动游戏即可。",
         ""
     };
+    private static final String AGREE_TEXT = "同意并继续";
+    private static final String DECLINE_TEXT = "拒绝并退出";
 
     private final JsonObject modpackJson;
     private final List<String> languagePacks;
