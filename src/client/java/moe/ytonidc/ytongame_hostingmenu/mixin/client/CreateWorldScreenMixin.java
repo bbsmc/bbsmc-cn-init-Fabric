e0c12ad0bda810cab1479e60cca0949a3ccb4094
Kotori0629 <79943751@qq.com>
2026-03-02 15:37:13 +0800

    fix: 联机开服 -> 联机开服(广告)，明确标识广告内容

--- src/client/java/moe/ytonidc/ytongame_hostingmenu/mixin/client/CreateWorldScreenMixin.java
+++ src/client/java/moe/ytonidc/ytongame_hostingmenu/mixin/client/CreateWorldScreenMixin.java
@@ -35,7 +35,7 @@ public abstract class CreateWorldScreenMixin extends Screen {
         int buttonY = 8;
 
         ytongame$hostingButton = new ButtonWidget(buttonX, buttonY, buttonWidth, buttonHeight,
-            new LiteralText("联机开服"),
+            new LiteralText("联机开服(广告)"),
             button -> this.client.setScreen(new HostingScreen(this)));
 
         this.addDrawableChild(ytongame$hostingButton);
