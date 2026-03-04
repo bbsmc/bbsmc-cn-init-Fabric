4d5a445eb28db379e5a943d863f3dd95b97806a1
Laotouy <18922569@qq.com>
2026-03-04 20:48:32 +0800

    重命名项目: 包路径改为net.bbsmc.cninit, Mod ID改为bbsmc-cn-init, 移除旧资源

---
+++ src/main/java/net/bbsmc/cninit/BbsmcCnInit.java
@@ -0,0 +1,10 @@
+package net.bbsmc.cninit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BbsmcCnInit implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
