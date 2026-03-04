c9f57023ab97425bd17850322d8e8042abc933e9
Laotouy <18922569@qq.com>
2026-03-04 20:48:32 +0800

    重命名项目: 包路径改为net.bbsmc.cninit, Mod ID改为bbsmc-cn-init, 移除旧资源

--- src/client/java/net/bbsmc/cninit/client/BbsmcCnInitDataGenerator.java
+++ src/client/java/net/bbsmc/cninit/client/BbsmcCnInitDataGenerator.java
@@ -1,9 +1,9 @@
-package moe.ytonidc.ytongame_hostingmenu.client;
+package net.bbsmc.cninit.client;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class Ytongame_hostingmenuDataGenerator implements DataGeneratorEntrypoint {
+public class BbsmcCnInitDataGenerator implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
